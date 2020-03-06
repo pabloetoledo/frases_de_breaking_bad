41c87c81462fa21f52c58d2919f9bb3c8bd11a89
unknown <toledopabloesteban@gmail.com>
2020-03-05 22:45:08 -0300

    primera version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-function App() {
+function Frase({frase}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="frase">
+      <h1>{frase.quote}</h1>
+      <p>-{frase.author} </p>
     </div>
-  );
+  )
+}
+
+
+function App(){
+
+  const [frase, obtenerFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    obtenerFrase(resultado.data[0]);
+  }
+
+  //siempre que se consulte una api se lo debe hacer en un useeffect
+  useEffect(
+    () => {
+      consultarAPI()
+    }, []
+  )
+
+  return (
+    <div className="contenedor"> 
+        <Frase
+          frase = {frase}
+        />
+        <button onClick={consultarAPI}>Generar una Nueva</button>
+    </div>
+  )
+  
 }
 
 export default App;
